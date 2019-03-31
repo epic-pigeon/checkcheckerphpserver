36ae03700ee0732f9982f030686bd282b4bdc273
nikita202 <nikita@ostaplyuk.com>
2019-03-31 20:23:10 +0300

    aaa

--- newapi.php
+++ newapi.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 $dbc = mysqli_connect("localhost", "checkchecker", "JJWMdF6riGuHDoVr", "checkchecker") or die("failed to connect to db");
 
 function generateRandomString($length) {
